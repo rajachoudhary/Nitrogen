0ccb620192bc98cd4f89f5bb0939df88ec92ef27
Vishal643 <vishalkumar199812@gmail.com>
2020-12-30 19:16:45 +0530

    complted 70% of the dashboard

--- js/index.js
+++ js/index.js
@@ -13,3 +13,29 @@ function createTemplate() {
 		tasktab.appendChild(ulTag);
 	}
 }
+
+// top nav profile dropdown
+let dropDownBtn = document.getElementById('dropbtn');
+dropDownBtn.addEventListener('click', myFunction);
+function myFunction() {
+	document.getElementById('myDropdown').classList.toggle('show');
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName('dropdown-content');
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+};
+
+// let timerDiv = document.getElementById('timer_dropbtn');
+// timerDiv.addEventListener('click', noBorder);
+// function noBorder() {
+// 	timerDiv.textDec
+// }
