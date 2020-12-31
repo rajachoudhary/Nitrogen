0002a24f9863bf93b55e285599af635b944fa7e3
Vishal643 <vishalkumar199812@gmail.com>
2020-12-31 12:36:01 +0530

    added some changes

--- js/index.js
+++ js/index.js
@@ -91,10 +91,10 @@ function myFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
 	if (!event.target.matches('.dropbtn')) {
-		var dropdowns = document.getElementsByClassName('dropdown-content');
-		var i;
+		let dropdowns = document.getElementsByClassName('dropdown-content');
+		let i;
 		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
+			let openDropdown = dropdowns[i];
 			if (openDropdown.classList.contains('show')) {
 				openDropdown.classList.remove('show');
 			}
@@ -102,8 +102,110 @@ window.onclick = function (event) {
 	}
 };
 
+// timer div dropdown
+// let timerdropDownBtn = document.getElementById('timer_dropbtn');
+// timerdropDownBtn.addEventListener('click', timerFunction);
+// function timerFunction() {
+// 	document.getElementById('timer_main-div').classList.toggle('show');
+// }
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function (event) {
+// 	if (!event.target.matches('.timer_dropbtn')) {
+// 		let timerdropdowns = document.getElementsByClassName('timer_main-div');
+// 		let i;
+// 		for (i = 0; i < timerdropdowns.length; i++) {
+// 			var opentimerDropdown = timerdropdowns[i];
+// 			if (opentimerDropdown.classList.contains('show')) {
+// 				opentimerDropdown.classList.remove('show');
+// 			}
+// 		}
+// 	}
+// };
 // let timerDiv = document.getElementById('timer_dropbtn');
 // timerDiv.addEventListener('click', noBorder);
 // function noBorder() {
 // 	timerDiv.textDec
+// }
+
+//timer code
+timerbtn = document.getElementById('timer_dropbtn');
+timerDiv = document.getElementById('timer_dropdown');
+timerbtn.addEventListener('click', getTimer);
+function getTimer(event) {
+	console.log('Hello');
+	let MaindivTag = document.createElement('div');
+	let firstDivInsideMainDiv = document.createElement('div');
+	let SecondDivInsideMainDiv = document.createElement('div');
+	MaindivTag.id = 'timer_main-div';
+	firstDivInsideMainDiv.id = 'first_div_inside_timer_main_div';
+	SecondDivInsideMainDiv.id = 'second_div_inside_timer_main_div';
+
+	MaindivTag.class = 'timer_main-div';
+	firstDivInsideMainDiv.class = 'first_div_inside_timer_main_div';
+	SecondDivInsideMainDiv.class = 'second_div_inside_timer_main_div';
+
+	MaindivTag.appendChild(firstDivInsideMainDiv);
+	MaindivTag.appendChild(SecondDivInsideMainDiv);
+	timerDiv.appendChild(MaindivTag);
+
+	//toggling divs
+	document.getElementById('timer_main-div').classList.toggle('show-timer');
+	if (!event.target.matches('.timer_dropbtn')) {
+		let timerdropdowns = document.getElementsByClassName('timer_main-div');
+		let i;
+		for (i = 0; i < timerdropdowns.length; i++) {
+			let openDropdown = timerdropdowns[i];
+			if (openDropdown.classList.contains('show-timer')) {
+				openDropdown.classList.remove('show-timer');
+			}
+		}
+	}
+}
+
+//Timer functionality code
+// var hrs = document.getElementById('hours');
+// var minutes = document.getElementById('minutes');
+// var seconds = document.getElementById('seconds');
+// var mili = document.getElementById('mili');
+
+// var miliSec = 0;
+// var sec = 0;
+// var minute = 0;
+// var hours = 0;
+// var stopWatch = null;
+// function display(hours, minute, sec, miliSec) {
+// 	hrs.textContent = hours;
+// 	minutes.textContent = minute;
+// 	seconds.textContent = sec;
+// 	mili.textContent = miliSec;
+// }
+// document.getElementById('start').addEventListener('click', startSW);
+// function startSW() {
+// 	stopWatch = setInterval(function () {
+// 		miliSec += 1;
+// 		if (miliSec >= 100) {
+// 			sec += 1;
+// 			miliSec = 0;
+// 		}
+// 		if (sec >= 60) {
+// 			minute += 1;
+// 			sec = 0;
+// 		}
+// 		if (minute >= 60) {
+// 			hours += 1;
+// 			minute = 0;
+// 		}
+// 		display(hours, minute, sec, miliSec);
+// 	}, 10);
+// }
+
+// document.getElementById('stop').addEventListener('click', stopTimer);
+// function stopTimer() {
+// 	clearInterval(stopWatch);
+// }
+
+// document.getElementById('reset').addEventListener('click', resetTimer);
+// function resetTimer() {
+// 	stopTimer();
+// 	display(0, 0, 0, 0);
 // }
